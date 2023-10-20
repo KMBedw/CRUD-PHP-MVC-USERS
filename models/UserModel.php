a1f7fcc669a193c13525809831d2a495daa51b0b
Edwin Koumba <edwin3@live.fr>
2023-10-20 15:05:13 +0200

    commentaires

--- models/UserModel.php
+++ models/UserModel.php
@@ -21,23 +21,20 @@ class UserModel {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    // Ajoute un nouvel utilisateur à la table "users" de la base de données.
     public function addUser($username, $email, $password) {
+        // Prépare la requête SQL pour insérer un nouvel utilisateur.
+        // Utilise des marqueurs de position "?" pour les valeurs à insérer.
         $stmt = $this->db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+        
+        // Vérifie si la préparation de la requête a échoué et, dans ce cas, affiche l'erreur MySQL
         if ($stmt === false) {
             die("MySQL Error: " . $this->db->error);
         }
-        $stmt->bind_param("sss", $username, $email, $password);
-        $stmt->execute();
-        $stmt->close();
-    }
-    
-
-    public function getUserById($id) {
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = ?");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
+        // Lie les variables aux marqueurs de position dans la requête préparée.
+        $stmt->bind_param("sss", $username, $email, $password); // "sss" signifie que les 3 paramètres sont des chaînes de caractères.
+        $stmt->execute();  // Exécute la requête préparée.
+        $stmt->close(); // Ferme l'objet de requête préparée.
     }
 
     public function updateUser($id, $username, $email) {
@@ -48,10 +45,28 @@ class UserModel {
     }
 
     public function deleteUser($id) {
+        // @param int $id L'ID de l'utilisateur à supprimer.
         $stmt = $this->db->prepare("DELETE FROM users WHERE id = ?");
+        // Lie la variable $id au marqueur de position dans la requête préparée.
+        // "i" signifie que le paramètre est un entier.
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->close();
+    }
+
+    
+    public function getUserById($id) {
+        /*
+        La fonction getUserById($id) est généralement utilisée lorsque vous avez besoin
+        d'obtenir des informations sur un utilisateur spécifique identifié par son ID.
+        Voici quelques cas d'utilisation typiques : Affichage de Profil, Contrôle d'Accès
+        Édition de l'utilisateur 
+         */   
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = ?");
+        $stmt->bind_param("i", $id); // "i" signifie que le paramètre est un entier.
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
     }
 }
 ?>
